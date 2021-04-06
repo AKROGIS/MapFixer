147164d51a1578747c71669a9acc81a8a5f92acd
Regan Sarwas <regan_sarwas@nps.gov>
2021-04-06 13:31:23 -0800

    Notes on pro implementation

--- MapFixer/MapFixer.cs
+++ MapFixer/MapFixer.cs
@@ -6,6 +6,17 @@ using ESRI.ArcGIS.Catalog;
 
 namespace MapFixer
 {
+
+    // Notes for Pro Implementation:
+    // Get all maps
+    //   foreach (var mpi in Project.Current.GetItems<MapProjectItem>()) { map = mpi.GetMap(); }
+    // get all broken layers for a map (ConnectionStatus is inherited from MapMember)
+    //   foreach (var layer = map.GetLayersAsFlattenedList().Where(l => l.ConnectionStatus == ConnectionStatus.Broken))
+    // fix layers by changing workspace path
+    //   layer.FindAndReplaceWorkspacePath(str1, str2, check)
+    // or by replacing datasource - need to build newDataset by creating new <Type>DataSource(connector) then OpenDataset<Type>(uri)
+   //   if (Layer.CanReplaceDataSource(newDataset)) { layer.ReplaceDataSource(newDataset); }
+
     public class MapFixer
     {
         public void FixMap(Moves moves)
